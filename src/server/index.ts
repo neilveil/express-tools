f53e42b919f2ffdbb91cf950bab3e8e9a8d8c460
Neil Arya <neilarya.in@gmail.com>
2025-03-22 21:09:15 +0530

    Import issues resolved

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,6 @@
-import { printStartLogs } from '@/helpers'
 import { Application } from 'express'
 import { Server } from 'http'
+import { printStartLogs } from '../helpers'
 
 export default (app: Application, port = 8080): Server => {
   return app.listen(port, () => printStartLogs(port))
